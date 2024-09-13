cdb13a45393d349eb5f9edeb89ffb56a998b1bc3
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-09-13 18:17:57 -0500

    Sell page

--- sick-fits/frontend/pages/orders.js
+++ sick-fits/frontend/pages/orders.js
@@ -1,4 +1,4 @@
-export default function Account() {
+export default function Orders() {
   return (
     <div>
       <p>Hello!</p>
--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,4 +1,4 @@
-export default function Products() {
+export default function Sell() {
   return (
     <div>
       <p>Hello!</p>
