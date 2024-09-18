ce71e911067b01f0a35e294de86567b1702096a4
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-09-18 17:32:45 -0500

    Styled component to create logo

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,17 +1,44 @@
 import Link from 'next/link';
-import React from 'react';
+import styled from 'styled-components';
 import Nav from './Nav';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  background: red;
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-templated-columns: auto 1fr;
+    justify-content: space-between;
+    align-item: center;
+  }
+`;
 
 export default function Header() {
   return (
-    <header>
+    <HeaderStyles>
       <div className="bar">
-        <Link href="/">Sick fits</Link>
+        <Logo>
+          <Link href="/">Sick fits</Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
       <Nav />
-    </header>
+    </HeaderStyles>
   );
 }
--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,11 +1,7 @@
-import Header from "../components/Header";
-
 export default function Sell() {
   return (
     <>
-        <Header/>
-        <p>Hello I am the sell page!</p>
+      <p>Hello I am the sell page!</p>
     </>
-      
-) 
+  );
 }
