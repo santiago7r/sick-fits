58fc85c10a04dd4619767a26bab3957b07b245af
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-10-10 14:37:08 -0500

    Query for single param

--- sick-fits/frontend/pages/product/[id].js
+++ sick-fits/frontend/pages/product/[id].js
@@ -1,6 +1,21 @@
+import { useQuery } from '@apollo/client'
+import gql from 'graphql-tag';
 import React from 'react'
 
+const SINGLE_ITEM_QUERY = gql`
+    query {
+        Product(where: {
+            id : "66f2e94e6766677a359ea639"
+        }) {
+            name
+            price
+            description
+        }
+    }
+`;
+
 export default function SingleProduct({ query }) {
+  const { data, loading, error } = useQuery(SINGLE_ITEM_QUERY);
   return (
     <p>
       This is a single product {query.id}
